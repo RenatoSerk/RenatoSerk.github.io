a26ed2cd2dbfc36ec88b40295e2cdb5ab6aeb806
Renato Serkhanian <brickworked@gmail.com>
2025-07-18 17:15:13 -0400

    Add mars model to scene

--- src/landing/LandingScene.tsx
+++ src/landing/LandingScene.tsx
@@ -2,14 +2,15 @@ import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { useEarthRotation } from './hooks/useEarthRotation';
 import { useThree } from '@react-three/fiber';
-import { EarthModel } from './Models';
+import { EarthModel, MarsModel } from './Models';
 
 const LandingScene: React.FC = () => {
   const earthRef = useRef<THREE.Group>(null!);
-  const glowRef = useRef<THREE.Mesh>(null!);
+  const marsRef = useRef<THREE.Group>(null!);
+  const earthGlowRef = useRef<THREE.Mesh>(null!);
   const { camera } = useThree();
 
-  useEarthRotation(earthRef, glowRef);
+  useEarthRotation(earthRef, earthGlowRef);
 
   useEffect(() => {
     camera.position.set(-3, 2, 5);
@@ -20,13 +21,21 @@ const LandingScene: React.FC = () => {
   return (
     <>
       <directionalLight position={[3, 2, 10]} intensity={1.5} />
+
+      // Earth
       <group ref={earthRef} position={[4, 2, -6]} scale={5}>
         <EarthModel />
       </group>
-      <mesh ref={glowRef} position={[4, 2, -6]} scale={5.5} visible={false}>
+      <mesh ref={earthGlowRef} position={[4, 2, -6]} scale={5.5} visible={false}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshBasicMaterial color="white" transparent opacity={0.15}/>
       </mesh>
+
+      // Mars
+      <group ref={marsRef} position={[4, -10, -6]} scale={40}>
+        <MarsModel />
+      </group>
+      
     </>
   );
 };
